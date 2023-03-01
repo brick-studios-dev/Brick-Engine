f7aa80ad1951b19e3194d06059fe9cf770bab043
Ailakks <hola@cortmnzz.com>
2023-03-01 22:11:17 +0100

    added sequence loader list

--- src/main/java/com/cortmnzz/brickengine/sequence/SequenceLoader.java
+++ src/main/java/com/cortmnzz/brickengine/sequence/SequenceLoader.java
@@ -7,13 +7,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class SequenceLoader {
     private static final Configuration configuration = BrickEngine.getInstance().getSequencesConfiguration();
     private static final ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
 
-    public SequenceLoader() {
+    private static final List<SequenceAction> sequenceActionList = new ArrayList<>();
+
+    static {
         Optional.ofNullable(configuration.getConfigurationSection("enabled")).ifPresent(enabled -> {
             enabled.getKeys(false).forEach(section -> {
                 try {
@@ -29,7 +33,7 @@ public class SequenceLoader {
                         });
                     });
 
-                    sequenceAction.perform();
+                    sequenceActionList.add(sequenceAction);
                 } catch (JsonProcessingException exception) {
                     throw new RuntimeException(exception);
                 }
